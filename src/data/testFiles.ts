b179148a94bd09991eb2117ef3ab0fd1cc33ff94
Michelle Kaplan <kaplan.michelle7@gmail.com>
2024-05-22 23:29:49 -0600

    Add test data

---
+++ src/data/testFiles.ts
@@ -0,0 +1,18 @@
+import { File } from "../types";
+
+export const testFiles: File[] = [
+  {
+    id: 11,
+    name: "file1.exe",
+    device: "Wario",
+    path: "/path/to/file1.exe",
+    status: "Available",
+  },
+  {
+    id: 12,
+    name: "file2.pdf",
+    device: "Bowser",
+    path: "/path/to/file2.pdf",
+    status: "Scheduled",
+  },
+];
