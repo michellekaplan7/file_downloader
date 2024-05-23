2d7e4129311008ba8c21372af9f529fa36d63889
Michelle Kaplan <kaplan.michelle7@gmail.com>
2024-05-22 20:58:47 -0600

    Add files data

---
+++ src/data/files.ts
@@ -0,0 +1,46 @@
+import { File } from "../types";
+
+export const files: File[] = [
+  {
+    id: 1,
+    name: "smss.exe",
+    device: "Mario",
+    path: "\\Device\\HarddiskVolume2\\Windows\\System32\\smss.exe",
+    status: "Scheduled",
+  },
+  {
+    id: 2,
+    name: "netsh.exe",
+    device: "Luigi",
+    path: "\\Device\\HarddiskVolume2\\Windows\\System32\\netsh.exe",
+    status: "Available",
+  },
+  {
+    id: 3,
+    name: "uxtheme.dll",
+    device: "Peach",
+    path: "\\Device\\HarddiskVolume1\\Windows\\System32\\uxtheme.dll",
+    status: "Available",
+  },
+  {
+    id: 4,
+    name: "aries.sys",
+    device: "Daisy",
+    path: "\\Device\\HarddiskVolume1\\Windows\\System32\\aries.sys",
+    status: "Scheduled",
+  },
+  {
+    id: 5,
+    name: "cryptbase.dll",
+    device: "Yoshi",
+    path: "\\Device\\HarddiskVolume1\\Windows\\System32\\cryptbase.dll",
+    status: "Scheduled",
+  },
+  {
+    id: 6,
+    name: "7za.exe",
+    device: "Toad",
+    path: "\\Device\\HarddiskVolume1\\temp\\7za.exe",
+    status: "Scheduled",
+  },
+];
