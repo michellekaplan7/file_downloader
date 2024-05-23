7c6ed2ee3b8cea0c45a892b206ed06f08a507cd9
Michelle Kaplan <kaplan.michelle7@gmail.com>
2024-05-22 23:55:58 -0600

    Add tests for FileTableRow

---
+++ src/components/FileTableRow/FileTableRow.test.tsx
@@ -0,0 +1,115 @@
+import React from "react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import userEvent from "@testing-library/user-event";
+
+import { FileTableRow } from "./FileTableRow";
+import { testFiles } from "../../data/testFiles";
+
+const mockOnSelectItem = jest.fn();
+
+const testFile = testFiles[0];
+
+describe("FileTableRow Component", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test("renders without crashing", () => {
+    render(
+      <FileTableRow
+        file={testFile}
+        isSelected={false}
+        onSelectItem={mockOnSelectItem}
+      />
+    );
+  });
+
+  test("renders file data correctly", () => {
+    render(
+      <FileTableRow
+        file={testFile}
+        isSelected={false}
+        onSelectItem={mockOnSelectItem}
+      />
+    );
+
+    expect(screen.getByText(testFile.name)).toBeInTheDocument();
+    expect(screen.getByText(testFile.device)).toBeInTheDocument();
+    expect(screen.getByText(testFile.path)).toBeInTheDocument();
+    expect(screen.getByText(testFile.status)).toBeInTheDocument();
+  });
+
+  test("checkbox is checked when isSelected is true", () => {
+    render(
+      <FileTableRow
+        file={testFile}
+        isSelected={true}
+        onSelectItem={mockOnSelectItem}
+      />
+    );
+
+    const checkbox = screen.getByRole("checkbox");
+    expect(checkbox).toBeChecked();
+  });
+
+  test("checkbox is not checked when isSelected is false", () => {
+    render(
+      <FileTableRow
+        file={testFile}
+        isSelected={false}
+        onSelectItem={mockOnSelectItem}
+      />
+    );
+
+    const checkbox = screen.getByRole("checkbox");
+    expect(checkbox).not.toBeChecked();
+  });
+
+  test("calls onSelectItem with file id when CHECKBOX is clicked", () => {
+    render(
+      <FileTableRow
+        file={testFile}
+        isSelected={false}
+        onSelectItem={mockOnSelectItem}
+      />
+    );
+
+    const checkbox = screen.getByRole("checkbox");
+    userEvent.click(checkbox);
+
+    expect(mockOnSelectItem).toHaveBeenCalledWith(testFile.id);
+  });
+
+  test("calls onSelectItem with file id when ROW is clicked", () => {
+    render(
+      <FileTableRow
+        file={testFile}
+        isSelected={false}
+        onSelectItem={mockOnSelectItem}
+      />
+    );
+
+    const row = screen.getByRole("row");
+    userEvent.click(row);
+
+    expect(mockOnSelectItem).toHaveBeenCalledWith(testFile.id);
+  });
+
+  test("does NOT call onSelectItem from the row when click originates from checkbox", () => {
+    render(
+      <FileTableRow
+        file={testFile}
+        isSelected={false}
+        onSelectItem={mockOnSelectItem}
+      />
+    );
+
+    const checkbox = screen.getByRole("checkbox");
+    fireEvent.click(checkbox);
+
+    expect(mockOnSelectItem).toHaveBeenCalledTimes(1); // Only called once by the checkbox click
+  });
+
+  // TODO: fix console.error warning
+});
